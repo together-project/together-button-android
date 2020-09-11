bfe3f090ce1da38256a5af70ae1b7ad3d283de7e
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-09-10 23:11:35 -0300

    Updated libs in gradle and fixed test file

--- app/src/androidTest/java/com/github/togetherproject/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/togetherproject/MainActivityTest.kt
@@ -1,6 +1,6 @@
 package com.github.togetherproject
 
-import androidx.lifecycle.Lifecycle
+import android.Manifest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
@@ -11,21 +11,21 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Before
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
     @get: Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val permissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.CALL_PHONE)
 
-    @Before
-    fun before() {
-        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
-    }
+    @get: Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun test_isActivityInView() {
