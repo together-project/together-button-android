6bf4fa4554a0e3b5971ce773a2af4c6bbc472a6d
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-09-12 14:59:45 -0300

    Removed TogetherButtonLib from jitpack and added project lib.

---
+++ togetherbutton/src/main/java/com/github/togetherproject/button/Fragments/TogetherButtonFragment.kt
@@ -0,0 +1,54 @@
+package com.github.togetherproject.button
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.togetherproject.button.databinding.FragmentTogetherBottomSheetBinding
+import com.github.togetherproject.button.databinding.TogetherHeaderBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class TogetherButtonFragment : BottomSheetDialogFragment() {
+
+    private var dialog: BottomSheetDialog? = null
+    private lateinit var binding: FragmentTogetherBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTogetherBottomSheetBinding.inflate(inflater)
+
+        val fragmentManager = childFragmentManager
+        fragmentManager.beginTransaction().add(R.id.fragmentContainer, HomeFragment()).commit()
+        binding.headerLayout.imgBtnClose.setOnClickListener {
+            dialog?.hide()
+        }
+
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        super.onCreateDialog(savedInstanceState)
+
+        dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog?.behavior?.peekHeight = 0
+        dialog?.behavior?.isHideable = true
+        dialog?.behavior?.state = BottomSheetBehavior.STATE_EXPANDED
+        dialog?.behavior?.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) dialog?.hide()
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+
+        return dialog!!
+    }
+}
---
+++ togetherbutton/src/main/java/com/github/togetherproject/button/Utils/PermissionUltis.kt
@@ -0,0 +1,25 @@
+package com.github.togetherproject.button
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+object PermissionUltis {
+
+    fun requestPermissions(context: Context) {
+        if (!hasPermissions(context)) {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(Manifest.permission.CALL_PHONE),
+                1
+            )
+        }
+    }
+
+    fun hasPermissions(context: Context) =
+        context.checkCallingOrSelfPermission(Manifest.permission.CALL_PHONE) ==
+                PackageManager.PERMISSION_GRANTED
+
+}
