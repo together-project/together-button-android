b7e3ebd50dd71d940e737ba882c77faab6abb044
zampnrs <46901316+zampnrs@users.noreply.github.com>
2020-10-24 16:58:08 -0300

    fix(round bs and button style)

--- togetherbutton/src/main/java/com/github/togetherproject/button/module/setup/TogetherButtonFragment.kt
+++ togetherbutton/src/main/java/com/github/togetherproject/button/module/setup/TogetherButtonFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.togetherproject.button.R
 import com.github.togetherproject.button.databinding.FragmentTogetherBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -13,7 +14,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class TogetherButtonFragment(
     private var dialog: BottomSheetDialog? = null
 ): BottomSheetDialogFragment() {
-
+    
     private lateinit var binding: FragmentTogetherBottomSheetBinding
 
     override fun onCreateView(
@@ -35,21 +36,18 @@ class TogetherButtonFragment(
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.TogetherBottomSheetDialog)
 
         dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         dialog?.behavior?.apply {
-            peekHeight = 0
             isHideable = true
             state = BottomSheetBehavior.STATE_EXPANDED
-            addBottomSheetCallback(object :
-                BottomSheetBehavior.BottomSheetCallback() {
-
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
                     if (newState == BottomSheetBehavior.STATE_COLLAPSED) dialog?.hide()
                 }
 
-                override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                }
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
             })
         }
 
